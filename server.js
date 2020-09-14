85028d094fd55fb85cd4c15a589bb26af4ceb42d
Jason Kha <jasonkha63@yahoo.com>
2020-09-14 14:51:53 -0500

    Front/Back end finished

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -11,13 +12,20 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) =>
-    res.json({ msg: 'Welcome to the ContactKeeper API' })
-);
-
 // define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
+
+    // route to index.html
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    );
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
